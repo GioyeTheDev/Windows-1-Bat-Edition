80bd3a45481d0bb14a07007c904564e1b2ed4bbb
GioyeTheDev <138115736+GioyeTheDev@users.noreply.github.com>
2023-07-19 15:17:09 +0200

    1.4 But Better! Removed Linux, Added Updates, Bugs Fixed, MODS ADDED, Copyrighed.

---
+++ Windows 1.5 Bat Edition/C0m0d/Utility.bat
@@ -0,0 +1,56 @@
+@echo off
+color 1f
+cls
+echo Utility
+echo Opens All Utility APPS. close one, open one.
+calculator
+mspaint
+iexplore
+slidetoshutdown
+mspaint
+regedit
+echo Prydev is Nice
+calculator
+mspaint
+iexplore
+slidetoshutdown
+mspaint
+regedit
+echo Prydev is Nice
+calculator
+mspaint
+iexplore
+slidetoshutdown
+mspaint
+regedit
+echo Prydev is Nice
+calculator
+mspaint
+iexplore
+slidetoshutdown
+mspaint
+regedit
+echo Prydev is Nice
+calculator
+mspaint
+iexplore
+slidetoshutdown
+mspaint
+regedit
+echo Prydev is Nice
+calculator
+mspaint
+iexplore
+slidetoshutdown
+mspaint
+regedit
+echo Prydev is Nice
+calculator
+mspaint
+iexplore
+slidetoshutdown
+mspaint
+regedit
+echo Prydev is Nice
+echo Utility Is Out Of Utility.
+pause
---
+++ Windows 1.5 Bat Edition/C0m0d/lilexplorer2.bat
@@ -0,0 +1,9 @@
+@echo off
+cls
+color 20
+cd lilexplorer
+echo LILEXPLORER 2
+iexplorer
+iexplore
+ie
+echo Thank you!
